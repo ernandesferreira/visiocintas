13ba9b125cc29efb8d182a9c823663be8cfeb123
ernandesferreira <ernandes.dev@gmail.com>
2015-01-22 18:51:52 -0200

    construction

--- wp-content/themes/odin/index.php
+++ wp-content/themes/odin/index.php
@@ -1,28 +1,52 @@
-<?php
-/**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme and one of the
- * two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * For example, it puts together the home page when no home.php file exists.
- *
- * @link http://codex.wordpress.org/Template_Hierarchy
- *
- * @package Odin
- * @since 2.2.0
- */
 
-get_header(); ?>
+	<style type="text/css">
+	html{
+		background: rgb(169,219,128); /* Old browsers */
+		background: -moz-radial-gradient(center, ellipse cover,  rgba(169,219,128,1) 0%, rgba(150,197,111,1) 100%); /* FF3.6+ */
+		background: -webkit-gradient(radial, center center, 0px, center center, 100%, color-stop(0%,rgba(169,219,128,1)), color-stop(100%,rgba(150,197,111,1))); /* Chrome,Safari4+ */
+		background: -webkit-radial-gradient(center, ellipse cover,  rgba(169,219,128,1) 0%,rgba(150,197,111,1) 100%); /* Chrome10+,Safari5.1+ */
+		background: -o-radial-gradient(center, ellipse cover,  rgba(169,219,128,1) 0%,rgba(150,197,111,1) 100%); /* Opera 12+ */
+		background: -ms-radial-gradient(center, ellipse cover,  rgba(169,219,128,1) 0%,rgba(150,197,111,1) 100%); /* IE10+ */
+		background: radial-gradient(ellipse at center,  rgba(169,219,128,1) 0%,rgba(150,197,111,1) 100%); /* W3C */
+		filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#a9db80', endColorstr='#96c56f',GradientType=1 ); /* IE6-9 fallback on horizontal gradient */
+		overflow-x: hidden; 
+	}
+	body{
+		width: 100%;
+		margin: 0;
+		padding: 0;
+	}
+	.container{
+		width: 100%;
+		padding: 10px;
+		margin: 0 auto;
+		
 
+	}
+	.meio{
+		width: 970px;
+		margin: 0 auto;
+		background: url('wp-content/themes/odin/assets/images/logo_site.png') no-repeat top center;
+		background-size: contain;
+		height: 600px;
+		margin-top: 80px;
+		padding: 10px;
+	}
+	.text{
+		font-size: 30px;
+		color: #006E2E;
+	}
+	</style>
 	<div id="primary" class="<?php echo odin_classes_page_sidebar(); ?>">
 		<main id="main" class="site-main" role="main">
 
-			Index
+			<div class="container">
+				<div class="meio">
+					<p class="text"> Site em Construção </p>
+				</div>
+
+			</div>
 
 		</main><!-- #content -->
 	</div><!-- #primary -->
 
-<?php
-get_sidebar();
-get_footer();
