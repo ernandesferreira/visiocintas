a4d47bb6ec49b8178a2999e078b42245edf8a09e
ernandesferreira <ernandes.dev@gmail.com>
2015-02-03 12:07:18 -0200

    foi

--- wp-content/themes/odin/footer.php
+++ wp-content/themes/odin/footer.php
@@ -52,25 +52,15 @@
 											echo do_shortcode('[gravityform id="1" name="Fale Conosco" title="false" description="false" ajax="true"]');
 										?>
 									</div>
-<<<<<<< HEAD
-									<div class="row">
-										<div class="col-lg-12 col-md-12 col-xs-12 col-sm-12">
-											<div class="tel" style="  font-size: 35px;  color: #fff;  font-weight: 600;  margin-bottom: 10px;">
-												TEL.: (21) 3761-7272
-											</div>
-										</div>
-									</div>
-=======
-										
->>>>>>> 80f755c8ccd847f152147640359d06519dd51214
+
 								</main>
 							</div>
 						</div>
 						<div class="col-lg-12 col-md-12 col-xs-12 col-sm-12">
-											<div class="tel" style="">
-												TEL.: (21) 3761-7272
-											</div>
-										</div>
+							<div class="tel" style="">
+								TEL.: (21) 3761-7272
+							</div>
+						</div>
 					</div>
 			</section>
 			<div class="site-info">
